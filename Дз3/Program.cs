509fa31b1dffef640b5c41153ed9add4abef98e9
veloc1ty <odexta02@gmail.com>
2023-10-01 22:30:49 +0300

    Создал программу 3

---
+++ Дз3/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите цифру, обозначающую день недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num > 7)
+{
+    Console.WriteLine("Вы ввели некорректный день недели");
+}
+if(num == 1)
+{
+    Console.WriteLine("Нет");
+}
+else if(num == 2)
+{
+    Console.WriteLine("Нет");
+}
+else if(num == 3)
+{
+    Console.WriteLine("Нет");
+}
+else if(num == 4)
+{
+    Console.WriteLine("Нет");
+}
+else if(num == 5)
+{
+    Console.WriteLine("Нет");
+}
+else if(num == 6)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Да");
+}
