bbb535babd2573fee9034431cd17e298956648cb
veloc1ty <odexta02@gmail.com>
2023-10-01 22:20:53 +0300

    Сделал программу 1

---
+++ Дз1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num < 100 || num >= 1000)
+{
+    Console.WriteLine("Вы ввели не трёхзначное число");
+}
+int num2 = num / 10 % 10;
+Console.WriteLine(num2);
