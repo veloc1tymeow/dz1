d141ed49aab01add5bb70925ed759b15b476d23e
veloc1ty <odexta02@gmail.com>
2023-10-01 22:25:37 +0300

    Создал программу 2, исправил программу 1

--- Дз1/Program.cs
+++ Дз1/Program.cs
@@ -5,4 +5,4 @@ if(num < 100 || num >= 1000)
     Console.WriteLine("Вы ввели не трёхзначное число");
 }
 int num2 = num / 10 % 10;
-Console.WriteLine(num2);
+Console.WriteLine($"{"Вторая цифра числа - "}{num2}");
---
+++ Дз2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+while(num > 999)
+{
+    num /= 10;
+}
+int num2 = num % 10;
+Console.WriteLine($"{"Третья цифра числа - "}{num2}");
